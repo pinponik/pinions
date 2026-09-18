30014ed16d028756d2503ef26889b71941395254
Pinponik <256419204+pinponik@users.noreply.github.com>
2026-09-18 09:59:03 +0200

    Update run function to handle multiple windows and remove Flow parameter Simplify the API by removing the Flow parameter from pinions::run and moving flow control logic into the run function itself. Update Application to store multiple windows in an array and handle redraw requests for all windows.
    Adjust the counter example to use the new simplified run function
    signature.
    
    Signed-off-by: Pinponik <256419204+pinponik@users.noreply.github.com>

--- src/std_run.rs
+++ src/std_run.rs
@@ -7,18 +7,20 @@ use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
 struct Application<A: App<T, W>, const T: usize, const W: usize> {
-    window: Option<Window>,
+    windows: Vect<Option<Window>, W>,
     app: A,
     ctx: Ctx<T, W>,
 }
 
 impl<A: App<T, W>, const T: usize, const W: usize> ApplicationHandler for Application<A, T, W> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .unwrap(),
-        );
+        for i in 0..W {
+            self.windows[i] = Some(
+                event_loop
+                    .create_window(Window::default_attributes())
+                    .unwrap(),
+            );
+        }
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
@@ -26,14 +28,28 @@ impl<A: App<T, W>, const T: usize, const W: usize> ApplicationHandler for Applic
             WindowEvent::CloseRequested => {
                 self.ctx.should_close = true;
             }
-            WindowEvent::RedrawRequested => {}
+            WindowEvent::RedrawRequested => {
+                for window in self.windows.iter_mut() {
+                    window.as_ref().unwrap().request_redraw();
+                }
+            }
             _ => {}
         }
     }
 }
 
-pub fn run<const T: usize, const W: usize, A: App<T, W>>(app: &mut A, flow: Flow) {
-    let mut ctx = Ctx::new();
-    app.run(&mut ctx);
-    let event_loop = EventLoop::new();
+pub fn run<const T: usize, const W: usize, A: App<T, W>>(app: A) {
+    let mut appl = Application {
+        windows: Vect::new(),
+        app: app,
+        ctx: Ctx::new(),
+    };
+    appl.app.run(&mut appl.ctx);
+    let event_loop = EventLoop::new().unwrap();
+    if let Flow::Wait = appl.ctx.flow {
+        event_loop.set_control_flow(ControlFlow::Wait);
+    } else {
+        event_loop.set_control_flow(ControlFlow::Poll);
+    }
+    event_loop.run_app(&mut appl);
 }
